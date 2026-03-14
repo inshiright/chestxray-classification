bdf3f26d7eb747bb4273aafaa17f04d76eeaca80
KidSmithy <hanshuennyuen@gmail.com>
2026-03-15 01:35:31 +0800

    stuff_added to run hans_mode notebook

--- src/model.py
+++ src/model.py
@@ -1,6 +1,7 @@
 from models.generic_cv.EfficientNetV2_S.model import EfficientNetV2
 # from models.generic_cv.ConvNeXt-V2.model import ConvNeXtV2
 # from models.generic_cv.Swin_Transformer.model import SwinTransformer
+from models.medical_sota.Radjepa.model import RadJEPA
 from config import NUM_CLASSES, MODEL_NAME
 
 def get_model():
@@ -12,6 +13,9 @@ def get_model():
 
     elif MODEL_NAME == "swin":
         return SwinTransformer(NUM_CLASSES)
+    
+    elif MODEL_NAME == "radjepa":
+        return RadJEPA(NUM_CLASSES)
 
     else:
         raise ValueError("Unknown model")
