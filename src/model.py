c9496051bd1e51e838319cd82fc4cb34c29bff36
KidSmithy <hanshuennyuen@gmail.com>
2026-03-15 19:15:50 +0800

    added CheXFound

--- src/model.py
+++ src/model.py
@@ -1,6 +1,7 @@
 from models.generic_cv.EfficientNetV2_S.model import EfficientNetV2
 # from models.generic_cv.ConvNeXt-V2.model import ConvNeXtV2
 # from models.generic_cv.Swin_Transformer.model import SwinTransformer
+from models.medical_sota.CheXFound.model import CheXFound   
 from models.medical_sota.RadJEPA.model import RadJEPA
 from config import NUM_CLASSES, MODEL_NAME
 
@@ -13,6 +14,9 @@ def get_model():
 
     elif MODEL_NAME == "swin":
         return SwinTransformer(NUM_CLASSES)
+
+    elif MODEL_NAME == "chexfound":
+        return CheXFound(NUM_CLASSES, freeze_backbone=True)
     
     elif MODEL_NAME == "radjepa":
         return RadJEPA(NUM_CLASSES)
